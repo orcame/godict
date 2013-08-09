35b61c6e3c492f476b6c2b28b8d3b1d2f6102458
neeo <neeo@neeo-laptop.(none)>
2013-08-10 01:13:19 +0800

    support the similar words if the word not exist.

--- src/lunch.go
+++ src/lunch.go
@@ -16,9 +16,14 @@ type Acceptation struct{
 
 type Word struct{
 	Value string
+	Similar string
 	LookupCount int
 	Acceptations []Acceptation
 }
+
+var question_re=regexp.MustCompile(`(?m)<div id="question"(\w|\W)*?</div>`)
+
+var ul_re=regexp.MustCompile(`<ul(\w|\W)*?</ul>`)
 
 var group_pos_re =regexp.MustCompile(`(?m)<div class="group_pos">(\w|\W)*?</div>`)
 var pos_re =regexp.MustCompile(`(?m)<p>(\w|\W)*?</p>`)
@@ -26,20 +31,49 @@ var fl_re = regexp.MustCompile(`(?m)<strong class="fl">.*?</strong>`)
 var label_re=regexp.MustCompile(`<span class="label_list">(\w|\W)*?</span>`)
 
 var html_re = regexp.MustCompile("\\<[\\S\\s]+?\\>")  
-var space_re =regexp.MustCompile(`\s{2,}`)
+var space_re =regexp.MustCompile(`(\s|&nbsp;){2,}`)
 
 var history map[string]Word = make(map[string]Word)
 
+var colorEnd string="\x1b[0m"
+
+func printWithColor(value string,colorCode string,newline bool){
+	fmt.Printf(colorCode)
+	fmt.Printf(value)
+	fmt.Printf(colorEnd)
+	if newline{
+		fmt.Printf("\n")
+	}
+}
+
+// BIRed='\e[1;91m'        # Red
+func red(value string,newline bool){
+	printWithColor(value,"\x1b[1;91m",newline)
+}
+
+// BIGreen='\e[1;92m'      # Green
+func green(value string,newline bool){
+	printWithColor(value,"\x1b[1;92m",newline)
+}
+// BIYellow='\e[1;93m'     # Yellow
+func yellow(value string,newline bool){
+	printWithColor(value,"\x1b[1;93m",newline)
+}
+// BIBlue='\e[1;94m'       # Blue
+func blue(value string,newline bool){
+	printWithColor(value,"\x1b[1;94m",newline)
+}
+// BIPurple='\e[1;95m'     # Purple
+func purple(value string,newline bool){
+	printWithColor(value,"\x1b[1;95m",newline)
+}
+// BICyan='\e[1;96m'       # Cyan
+func cyan(value string,newline bool){
+	printWithColor(value,"\x1b[1;96m",newline)
+}
+
 func showError(err error){
-	fmt.Printf("\x1b[1;91m%s\x1b[0m\n",err)
-}
-
-func showPartOfSpeech(val string){
-	fmt.Printf("\x1b[1;92m"+val+"\x1b[0m")
-}
-
-func showMeaning(val string) {
-	fmt.Printf("\x1b[1;95m"+val+"\x1b[0m")	
+	red(err.Error(),true)
 }
 
 
@@ -62,9 +96,19 @@ func translate(word string) Word{
 		if err != nil{
 			fmt.Printf("%s",err)
 		}
-		group_pos:=group_pos_re.FindAllString(string(contents),-1)
+		contents_str :=string(contents)
+		question:=question_re.FindAllString(contents_str,-1)
+		if len(question)>0{
+			ul:=ul_re.FindAllString(question[0],-1)
+			if len(ul)>0{
+				similar:=html_re.ReplaceAllString(ul[0],"")
+				similar=space_re.ReplaceAllString(similar," ")
+				result.Similar=strings.TrimSpace(similar)
+			}
+		}
+		group_pos:=group_pos_re.FindAllString(contents_str,-1)
 		if len(group_pos)>0{
-			ps :=pos_re.FindAllString(string(group_pos[0]),-1)
+			ps :=pos_re.FindAllString(group_pos[0],-1)
 			pos_len:=len(ps)
 
 			accs:=make([]Acceptation,pos_len)
@@ -95,11 +139,28 @@ func translate(word string) Word{
 
 func showWord(word Word){
 	if word.LookupCount>0{
-		for _,val := range word.Acceptations{
-			showPartOfSpeech(val.PartOfSpeech)
-			showMeaning(val.Meaning)
-			fmt.Println()
+		yellow("",true)
+		notexist:=len(word.Similar)>0
+		if notexist{
+			sims:=strings.Split(word.Similar," ")
+			yellow(">>>>>>>>The world "+word.Value+" not exist, do you means ",false)
+			for _,val := range sims{
+				cyan("["+val+"], ",false)
+			}
+
+			yellow("\b\b?\n",true)
+			yellow("\tThe means of word ",false)
+			cyan(sims[0],false)
+			yellow(" is:\n",true)
 		}
+		for _,val := range word.Acceptations{
+			if notexist{
+				green("\t",false)				
+			}
+			green(val.PartOfSpeech,false)
+			purple(val.Meaning,true)
+		}
+		yellow("",true)
 	}
 }
 
@@ -108,7 +169,7 @@ func main(){
 	bio:=bufio.NewReader(os.Stdin)
 	for ;true;{
 		word=""
-		fmt.Printf("\ninput your word/>")
+		fmt.Printf("input your world/>")
 		line ,_,err:=bio.ReadLine()
 		if err !=nil{
 			showError(err)
